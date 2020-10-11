0ac227214ad64c9bbc45441766aef49ba564c275
muelver0002 <72661100+muelver0002@users.noreply.github.com>
2020-10-11 10:11:59 +0800

    initial hello world

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldCsharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
