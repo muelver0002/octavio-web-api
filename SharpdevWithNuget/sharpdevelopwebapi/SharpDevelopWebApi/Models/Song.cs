3f56c08aab44f395e7b634de1f8e0024e4cb0efc
muelver0002 <72661100+muelver0002@users.noreply.github.com>
2020-10-19 10:16:39 +0800

    Added new shardevwithnugetband replace the previous due to build error

---
+++ SharpdevWithNuget/sharpdevelopwebapi/SharpDevelopWebApi/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SharpDevelopWebApi.Models{
+
+
+public class Song{
+
+public int Id { get; set; }
+public string Title { get; set; }
+public string Artist { get; set; }
+public string Genre { get; set; }
+}
+
+}
+
+
